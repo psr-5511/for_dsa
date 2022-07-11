b6c481da1b814b69ecf08a421b137b301f667cd6
Pranay Sharma <pranay07417@gmail.com>
2022-07-11 21:22:23 +0530

    same_tree

--- LinkedList/same_tree.cpp
+++ LinkedList/same_tree.cpp
@@ -1,55 +1,57 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-class TreeNode
-{
-public:
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+struct node {
+  int data;
+  struct node * left, * right;
 };
 
-class Solution {
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL || q==NULL) return (p==q);
-            
-        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
-    }
-};
+bool isIdentical(node * node1, node * node2) {
+  if (node1 == NULL && node2 == NULL)
+    return true;
+  else if (node1 == NULL || node2 == NULL)
+    return false;
 
-TreeNode * newNode(int data)
-{
-    TreeNode *node = new TreeNode();
-    node->val = data;
-    node->left = NULL;
-    node->right = NULL;
-
-    return node;
-
+  return ((node1 -> data == node2 -> data) && isIdentical(node1 -> left, node2 -> left) && isIdentical(node1 -> right, node2 -> right));
 }
 
-int main()
-{
-     TreeNode * root1 = newNode(1);
-     root1 -> left = newNode(2);
+struct node * newNode(int data) {
+  struct node * node = (struct node * ) malloc(sizeof(struct node));
+  node -> data = data;
+  node -> left = NULL;
+  node -> right = NULL;
+
+  return (node);
+}
+
+int main() {
+
+  struct node * root1 = newNode(1);
+  root1 -> left = newNode(2);
   root1 -> right = newNode(3);
   root1 -> right -> left = newNode(4);
   root1 -> right -> right = newNode(5);
 
-   TreeNode * root2 = newNode(1);
+  struct node * root2 = newNode(1);
   root2 -> left = newNode(2);
   root2 -> right = newNode(3);
   root2 -> right -> left = newNode(4);
-    root2 -> right -> right = newNode(5);
 
-
-    Solution obj ;
-  if (obj.isSameTree(root1, root2))
+  if (isIdentical(root1, root2))
     cout << "Two Trees are identical";
   else cout << "Two trees are non-identical";
-    return 0;
+
+  return 0;
 }
+// Output:
+
+// Two trees are non-identical
+
+// Time Complexity: O(N).
+
+// Reason: We are doing a tree traversal.
+
+// Space Complexity: O(N)
+
+// Reason: Space is needed for the recursion stack. In the worst case (skewed tree), space complexity can be O(N).
