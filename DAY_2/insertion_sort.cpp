e6d6d9d186e783ecc2b3da04b9bfa3e497ac8273
Pranay Sharma <pranay07417@gmail.com>
2022-07-19 00:10:50 +0530

    insertion sort

---
+++ DAY_2/insertion_sort.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int a[],int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = a[i];
+        int j = i-1;
+        for (; j >=0; j--)
+        {
+            if(a[j]>temp)
+                a[j+1]=a[j];
+
+            else 
+                break;
+        }
+
+        a[j+1]=temp;
+        
+    }
+    
+}
+
+void printArray(int arr[], int n) 
+{ 
+    int i; 
+    for (i = 0; i < n; i++) 
+        cout << arr[i] << " "; 
+    cout << endl;
+} 
+
+int main() 
+{
+
+    int arr[] = { 12, 11, 13, 5, 6 }; 
+    int N = sizeof(arr) / sizeof(arr[0]); 
+
+    insertion_sort(arr, N);
+
+    printArray(arr,N);
+    return 0;
+}
