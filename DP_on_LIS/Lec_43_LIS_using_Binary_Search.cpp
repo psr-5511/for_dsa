fe6b569c7b4a8a4906f668d01de7690adc368177
Pranay Sharma <pranay07417@gmail.com>
2022-09-11 11:12:39 +0530

    dp on LIS

---
+++ DP_on_LIS/Lec_43_LIS_using_Binary_Search.cpp
@@ -0,0 +1,31 @@
+//
+// Created by honey on 11-09-2022.
+//
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+int LIS_using_BS(vector<int> &arr, int n )
+{
+    vector<int> temp;
+    temp.push_back(arr[0]);
+    int len =1 ;
+    for (int i = 1; i < n; ++i) {
+        if(arr[i]>temp.back()){
+            temp.push_back(arr[i]);
+            len++;
+        }
+        else{
+            int ind = lower_bound(temp.begin(),temp.end(),arr[i]) - temp.begin();
+            temp[ind] = arr[i];
+        }
+    }
+    return len ;
+}
+int main()
+{
+    vector<int> arr =  {10,9,2,5,3,7,101,18};
+    int n = arr.size();
+    cout<<"The length of the longest increasing subsequence :"<<LIS_using_BS(arr,n);
+    return 0;
+}
+
